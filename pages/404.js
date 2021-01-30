24fdd08ddce416598d8a24865924ad52d66b9d39
Reasin <bjkk1290235@gn.iwasaki.ac.jp>
2021-01-30 16:35:25 +0900

    add:homeに戻れるようにしました

--- pages/404.js
+++ pages/404.js
@@ -1,9 +1,14 @@
+import Link from 'next/link'
+
 export default function Custom404(){
     return(
         <main className="main">
             <p>
                 ページがありません。
             </p>
+            <Link href="/">
+                <a>Homeに戻る</a>
+            </Link>
         </main>
     );
 }
